c7e79f977f0f3706faf805e3e8e70ecfffd452e9
ApostolOleg <apostololeg0@gmail.com>
2025-12-06 21:38:59 +0200

    last commit
    Co-authored-by: Dmytrii Penkivskiy <dimpennn@users.noreply.github.com>

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router";
 import Description from "../components/QuizzesUI/Description";
 
 const quizButtonStyle =
-	"bg-[rgb(233,14,178)] aspect-square rounded-3xl max-h-[400px] w-full text-1/2 flex items-center justify-center text-center hover:scale-105 transition-all shadow-[0_0_10px_rgba(114,0,104,0.692)]";
+	"bg-[rgb(233,14,178)] rounded-3xl max-h-[400px] max-w-[400px] aspect-square w-full text-1/2 flex items-center justify-center text-center hover:scale-105 transition-all shadow-[0_0_10px_rgba(114,0,104,0.692)]";
 
 export const containerStyle =
 	"w-[95%] m-0 mx-auto mb-5 bg-[rgb(34,14,34)] p-5 rounded-2xl shadow-[0_0_10px_rgba(114,0,104,0.692)]";
@@ -16,7 +16,7 @@ export default function Quizzes() {
 	const quizzes = getStorage().quizzes;
 
 	return (
-		<div className={containerStyle + " grid gap-[2vw] grid-cols-4 justify-center"}>
+		<div className={containerStyle + " grid gap-[2vw] grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6"}>
 			<Link to="/create" id={`quiz-add`} className={quizButtonStyle}>
 				<img src={addIcon} alt="Add Quiz" className="w-1/2 h-1/2" />
 			</Link>
