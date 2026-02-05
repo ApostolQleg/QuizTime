794108763816968a89126f21af0fa6ab32dcd362
ApostolOleg <apostololeg0@gmail.com>
2026-02-05 13:50:56 +0200

    delete analitics

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
-import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-		<Analytics />
 	</StrictMode>,
 );
