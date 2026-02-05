cc8b6a938fdd7de0855a0b84568d9b93c0cdf9a1
ApostolOleg <apostololeg0@gmail.com>
2026-02-05 13:39:50 +0200

    deleted unnecessary shi

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</StrictMode>
+		<Analytics />
+	</StrictMode>,
 );
