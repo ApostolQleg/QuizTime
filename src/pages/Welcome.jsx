1695dc546cb6fb9cb24e5de0e68b291f1e9b0a99
ApostolOleg <apostololeg0@gmail.com>
2026-05-30 19:57:22 +0300

    refactor: update Welcome page to use button component and improve styling

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,20 +1,26 @@
-import Button from "@/shared/ui/Button.jsx";
+import { useNavigate } from "react-router";
 
 export default function Welcome() {
+	const navigate = useNavigate();
+
 	return (
 		<div className="flex min-h-[85vh] flex-col items-center justify-start px-4">
 			<div className="max-w-4xl w-full space-y-6 opacity-0 animate-welcome">
-				<div className="flex flex-col sm:flex-row sm:items-end gap-9">
+				<div className="flex flex-1 flex-row sm:items-center gap-4">
 					<h1 className="text-7xl font-black tracking-tighter sm:text-9xl text-(--col-text-main) leading-none select-none">
 						QuizTime
 					</h1>
-
-					<Button
-						to="/quizzes"
-						className="auth-button text-2xl px-8 py-4 font-bold tracking-wider uppercase border-2 border-(--col-border) mb-1"
-					>
-						Start
-					</Button>
+					<div className="flex flex-1 justify-center ">
+						<button
+							type="button"
+							className="passive-button px-10! sm:px-6! py-1! sm:py-2!"
+							onClick={() => {
+								navigate("/quizzes");
+							}}
+						>
+							Start
+						</button>
+					</div>
 				</div>
 
 				<p className="text-xl sm:text-2xl text-(--col-text-muted) max-w-3xl text-left font-medium leading-relaxed pt-2">
--- src/shared/ui/auth/AuthActions.jsx
+++ src/shared/ui/auth/AuthActions.jsx
@@ -3,16 +3,11 @@ import { Link } from "react-router-dom";
 export default function AuthActions({ onClick }) {
 	return (
 		<div className="flex gap-3">
-			<Link
-				to="/register"
-				onClick={onClick}
-				className="auth-button"
-				style={{ backgroundColor: "var(--col-primary)" }}
-			>
+			<Link to="/register" onClick={onClick} className="button px-4! py-2!">
 				Sign Up
 			</Link>
 			<span className="text-(--col-text-muted) self-center text-xs">or</span>
-			<Link to="/login" onClick={onClick} className="auth-button">
+			<Link to="/login" onClick={onClick} className="passive-button px-4! py-2!">
 				Log In
 			</Link>
 		</div>
