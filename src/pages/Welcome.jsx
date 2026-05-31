3cae36d546f17b567b15ddda974425ff70908b6c
dimpennn <dmitrijpenkivskij@gmail.com>
2026-05-31 19:00:32 +0300

    feat: add 3D icon to Welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,7 +4,8 @@ export default function Welcome() {
 	const navigate = useNavigate();
 
 	return (
-		<div>
+		<div className="flex justify-center items-center"> 
+			<div>
 			<p>What time is it? It's QuizTime!</p>
 			<button
 				type="button"
@@ -14,6 +15,14 @@ export default function Welcome() {
 			>
 				Start
 			</button>
+			</div>
+				<model-viewer
+				    className="h-100 w-100"
+					src="/3DModels/QuizTime_logo.glb"
+					alt="A descriptive 3D model text for accessibility"
+					auto-rotate
+					camera-controls
+				/>
 		</div>
 	);
 }
